2ca14849d31c82fed644030ac91e012ef72d5af9
Joanna Colson <joannacolson@gmail.com>
2017-05-11 20:00:21 -0700

    completed work

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 angular.module('CardsAgainstAssembly', ['CardsApp'])
     .controller("CardsCtrl", ["$scope", "QuestionsFactory", function($scope, QuestionsFactory) {
         var questionCards = QuestionsFactory.getCards();
-        $scope.numPlayers = 2;
+        $scope.numPlayers = 3;
 
         $scope.newQuestion = function() {
             $scope.displayCard = questionCards[pickCardIndex(questionCards.length)].question;
-        }
+        };
 
-        $scope.newQuestion();
+        // $scope.newQuestion();
     }]);
 
 function pickCardIndex(deckSize) {
